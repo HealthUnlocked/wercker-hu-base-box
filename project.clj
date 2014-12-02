faa4d09467b7dd3d8a0911bd99019cafd5fb732c
William Roe <git@wjlr.org.uk>
2014-12-02 14:16:30 +0000

    Cache clojure versions

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject just-deps "0.0.1-SNAPSHOT"
+  :description "Just for caching dependencies that might be required"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.6.0"]])
